7a3c4e26684e495290e8367339906031d6f12a92
隔壁小胡 <79695586+gbxh@users.noreply.github.com>
2022-09-04 13:07:52 +0800

    Update Yunzai3withBaota-ubuntu.sh

--- Yunzai3withBaota-ubuntu.sh
+++ Yunzai3withBaota-ubuntu.sh
@@ -1,6 +1,6 @@
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh ed8484bec
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo apt update && sudo apt install nodejs -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo apt install redis redis-server -y
 sudo apt install chromium-browser -y
 sudo apt install libgbm-dev libasound2-dev -y
@@ -22,6 +22,8 @@ sudo git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 sudo mkdir ffmpeg/build && cd ffmpeg/build
 sudo ../configure --enable-gpl --enable-version3 --enable-nonfree --disable-ffplay --disable-ffprobe --enable-libopencore-amrnb --enable-libopencore-amrwb
 sudo make && sudo make install
+sudo echo "/usr/local/lib" >> /etc/ld.so.conf
+sudo ldconfig
 sudo apt install -y zlib1g zlib1g-dev libffi-dev openssl libssl-dev
 cd /usr/local/src
 sudo wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tar.xz
@@ -35,15 +37,9 @@ cd ~/Yunzai-Bot
 git clone https://github.com/yoimiya-kokomi/miao-plugin ./plugins/miao-plugin
 npm install moment
 cd ~/Yunzai-Bot
-git clone https://github.com/lcwf/python-plugin.git ./plugins/python-plugin
-npm install node-schedule
-cd ~/Yunzai-Bot/plugins/python-plugin
-pip install -r requirement.txt
-echo export PATH="~/.local/bin:$PATH" >> ~/.bashrc
-source ./bashrc
-cd ~/Yunzai-Bot
-sudo npm install cnpm -g
-cnpm install
+npm install image-size
+npm install pnpm -g
+pnpm install -P
 cd ~/Yunzai-Bot
 redis-server --save 900 1 --save 300 10 --daemonize yes
 node app
